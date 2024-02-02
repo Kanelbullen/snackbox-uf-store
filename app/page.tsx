0c6607aa7a90da0219b8329f69438558920e9c39
Joar Maltesson <joar05@hotmail.com>
2024-02-02 10:59:43 +0100

    Chore: update metadata

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,8 @@ import { Suspense } from 'react';
 export const runtime = 'edge';
 
 export const metadata = {
-  description: 'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
+  description:
+    'Vi är ett ungdomsföretag som vill göra det lättare att äta nyttigt. Vi säljer färdigkomponerade lådor med nyttigare alternativ.',
   openGraph: {
     type: 'website'
   }
