83a8b66ac74361020b18fef96c3df1e1ca2e6f50
Joar Maltesson <joar05@hotmail.com>
2024-02-22 11:04:17 +0100

    fix: grammatik

--- app/error.tsx
+++ app/error.tsx
@@ -5,7 +5,7 @@ import { Button } from 'components/ui/button';
 export default function Error({ reset }: { reset: () => void }) {
   return (
     <div className="mx-auto my-4 flex max-w-xl flex-col rounded-lg border border-border p-8 md:p-12">
-      <h2 className="text-xl font-bold">Oh nej!</h2>
+      <h2 className="text-xl font-bold">Åh nej!</h2>
       <p className="my-2">
         Ett problem uppstod med våran storefront. Detta kan vara ett temporärt problem, vänligen
         testa igen.
